509b0aeec27dfd9d3e177f6e5314dcd94c6b7d1a
Wouter van Itterzon <wouter.van.itterzon@gmail.com>
2021-10-14 17:01:40 +0200

    - preview voor video toegevoegd - mogelijkheid tot screenshare toegevoegd

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 
+enum Source {
+  webcam,
+  screencap
+}
+
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+
+  captureSource = Source
 
   localStream: any
 
@@ -16,23 +24,18 @@ export class MainComponent implements OnInit {
 
 
   dataChannel: any;
-  textToSend: any; 
+  textToSend: any;
 
   mediaStreamConstraints = {
-  video: true,
-  audio: true
-};
+    video: true,
+    audio: true
+  };
 
-connection  : any;
-
-
-
-
+  connection: any;
 
 
   constructor() {
-    this.connection = new RTCPeerConnection({ iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] })
-
+    this.connection = new RTCPeerConnection({iceServers: [{urls: 'stun:stun.l.google.com:19302'}]})
 
 
   }
@@ -40,104 +43,139 @@ connection  : any;
   ngOnInit(): void {
     console.log(this.connection)
 
-    this.connection.ondatachannel = (event:any) => {
-      this.dataChannel =  event.channel
-      this.dataChannel.onmessage = (event : any) => alert(event.data)
+    this.connection.ondatachannel = (event: any) => {
+      this.dataChannel = event.channel
+      this.dataChannel.onmessage = (event: any) => alert(event.data)
     }
 
-    this.connection.onconnectionstatechange = (event : any) => console.log( this.connection.connectionState); // console.log('onconnectionstatechange', connection.connectionState)
-    this.connection.oniceconnectionstatechange = (event : any)  => console.log( this.connection.iceConnectionState)  // console.log('oniceconnectionstatechange', connection.iceConnectionState)
+    this.connection.onconnectionstatechange = (event: any) => console.log(this.connection.connectionState); // console.log('onconnectionstatechange', connection.connectionState)
+    this.connection.oniceconnectionstatechange = (event: any) => console.log(this.connection.iceConnectionState)  // console.log('oniceconnectionstatechange', connection.iceConnectionState)
 
-    this.connection.onicecandidate = (event : any) => {
+    this.connection.onicecandidate = (event: any) => {
       // window.alert(event)
       // console.log('onicecandidate', event)
       if (!event.candidate) {
-          this.receivedOffer = this.connection.localDescription;
+        this.receivedOffer = this.connection.localDescription;
 
       }
-  }
+    }
 
   }
 
-  async  getMedia() {
+  async getMedia(captureSource: any) {
+
+    console.log(captureSource)
+
     let stream = null;
-    // mediastream openen 
-    try {
-      stream = await navigator.mediaDevices.getUserMedia(this.mediaStreamConstraints);
-      this.localStream = stream;
-      console.log(this.localStream)
-    // mediastreams toevoegen aan peer connection 
-      this.localStream.getTracks().forEach((track : any) => {
-      this.connection.addTrack(track)
-    });
-    } catch(err) {
-      console.log(err)
-    } 
+    // mediastream openen
+
+    switch (captureSource) {
+      case 0: {
+        console.log('webcam')
+
+        try {
+          this.localStream = await navigator.mediaDevices.getUserMedia(this.mediaStreamConstraints);
+          console.log(this.localStream)
+
+          // mediastreams toevoegen aan peer connection
+          this.localStream.getTracks().forEach((track: any) => {
+            this.connection.addTrack(track)
+          });
+        } catch (err) {
+          console.log(err)
+        }
+      }
+        break;
+      case 1: {
+        console.log('screencap')
+
+        try {
+          // @ts-ignore
+          this.localStream = await navigator.mediaDevices.getDisplayMedia(this.mediaStreamConstraints);
+          console.log(this.localStream)
+
+          // mediastreams toevoegen aan peer connection
+          this.localStream.getTracks().forEach((track: any) => {
+            this.connection.addTrack(track)
+          });
+        } catch (err) {
+          console.log(err)
+        }
+
+
+
+      }
+        break;
+      default: {
+        console.log('???')
+      }
+    }
   }
 
-  async closeMedia(){
 
-    // mediastreams sluiten 
+  async closeMedia() {
+
+    // mediastreams sluiten
     try {
-      this.localStream.getTracks().forEach((track: any) =>  {
+      this.localStream.getTracks().forEach((track: any) => {
         track.stop();
         // en uit peer connection verwijderen
         this.localStream.removeTrack(track);
       });
 
-    } catch(err){
+    } catch (err) {
       console.log(err)
     }
-    
+
 
   }
 
-async terminateConnection(){
-  this.connection.close();
-  this.connection = new RTCPeerConnection({ iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] })
-}
+  async terminateConnection() {
+    this.connection.close();
+    this.connection = new RTCPeerConnection({iceServers: [{urls: 'stun:stun.l.google.com:19302'}]})
+  }
 
-async initDataChannel(){
-  this.dataChannel = await this.connection.createDataChannel('data')
-  console.log(this.dataChannel)
-}
+  async initDataChannel() {
+    this.dataChannel = await this.connection.createDataChannel('data')
+    console.log(this.dataChannel)
+  }
 
-async  createOfferAndSetLocalDescription(){
-  await this.initDataChannel()
-  const offer = await this.connection.createOffer()
-  await this.connection.setLocalDescription(offer)
-  console.log(this.connection)
-}
+  async createOfferAndSetLocalDescription() {
+    await this.initDataChannel()
+    const offer = await this.connection.createOffer()
+    await this.connection.setLocalDescription(offer)
+    console.log(this.connection)
+  }
 
-async acceptRemoteOffer(){
-  console.log(this.receivedOffer)
-  await this.connection.setRemoteDescription(JSON.parse(this.receivedOffer))
-  console.log(this.connection)
-}
+  async acceptRemoteOffer() {
+    console.log(this.receivedOffer)
+    await this.connection.setRemoteDescription(JSON.parse(this.receivedOffer))
+    console.log(this.connection)
+  }
 
-async createAnswer(){
-  this.connection.onicecandidate = (event: any) => {
-  console.log('onicecandidate', event)
-    if (!event.candidate) {
+  async createAnswer() {
+    this.connection.onicecandidate = (event: any) => {
+      console.log('onicecandidate', event)
+      if (!event.candidate) {
 
+      }
     }
-}
-  const answer = await this.connection.createAnswer()
-  await this.connection.setLocalDescription(answer)
-}
+    const answer = await this.connection.createAnswer()
+    await this.connection.setLocalDescription(answer)
+  }
 
-async acceptRemoteAnswer(){
-  await this.connection.setRemoteDescription(JSON.parse(this.receivedAnswer))
+  async acceptRemoteAnswer() {
+    await this.connection.setRemoteDescription(JSON.parse(this.receivedAnswer))
 
-}
+  }
 
-sendText(){
-  this.dataChannel.send(this.textToSend)
-}
+  sendText() {
+    this.dataChannel.send(this.textToSend)
+  }
 
-logConnectionStatus(){
-  console.log(this.connection)
-}
+  logConnectionStatus() {
+    console.log(this.connection)
+  }
 
 
 }
